1d7c1141003bb3b92391a44037e87f7b731785d5
p72b <willy.riechert@gmail.com>
2018-11-26 20:24:53 +0100

    Made Location nullable in listener param.

--- locator/src/main/java/de/p72b/locator/location/ILastLocationListener.kt
+++ locator/src/main/java/de/p72b/locator/location/ILastLocationListener.kt
@@ -3,7 +3,7 @@ package de.p72b.locator.location
 import android.location.Location
 
 interface ILastLocationListener {
-    fun onLastLocationSuccess(location: Location)
+    fun onLastLocationSuccess(location: Location?)
 
     fun onLastLocationFailure(message: String?)
 }
