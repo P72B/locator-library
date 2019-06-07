2284387c8835353944920c5ca8711c4dfbd2b6d2
Martin Breuer <mbwarrenfaith@gmail.com>
2019-06-07 11:33:15 +0200

    Moved the implementation details from the three Activities to a dedicated implementation class to remove duplicated code

--- locator/src/main/java/de/p72b/locator/location/LocationAwareActivity.kt
+++ locator/src/main/java/de/p72b/locator/location/LocationAwareActivity.kt
@@ -5,32 +5,33 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 
-import de.p72b.locator.preferences.LocatorPreferences
-
 @SuppressLint("Registered")
 open class LocationAwareActivity : Activity() {
-    protected lateinit var locationManager: LocationManager
-    private lateinit var settingsClientManager: SettingsClientManager
+    private var activityImplementation = LocationAwareActivityImpl()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        settingsClientManager = SettingsClientManager(this)
-        locationManager = LocationManager(this, settingsClientManager)
+        activityImplementation.onCreate(this, savedInstanceState)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int,
                                             permissions: Array<String>,
                                             grantResults: IntArray) {
-        for (permission in permissions) {
-            LocatorPreferences.writeToPreferences(permission, true)
-        }
-        locationManager.notifyPermissionRequestResults(permissions, grantResults)
+        activityImplementation.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == SettingsClientManager.REQUEST_CODE_SETTINGS) {
-            settingsClientManager.onActivityResult(resultCode)
-        }
+        activityImplementation.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        activityImplementation.onStart(this)
+    }
+
+    override fun onStop() {
+        activityImplementation.onStop(this)
+        super.onStop()
     }
 }
--- locator/src/main/java/de/p72b/locator/location/LocationAwareFragmentActivity.kt
+++ locator/src/main/java/de/p72b/locator/location/LocationAwareFragmentActivity.kt
@@ -1,33 +1,37 @@
 package de.p72b.locator.location
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
-import de.p72b.locator.preferences.LocatorPreferences
 
+@SuppressLint("Registered")
 open class LocationAwareFragmentActivity : FragmentActivity() {
-    protected lateinit var locationManager: LocationManager
-    private lateinit var settingsClientManager: SettingsClientManager
+    private var activityImplementation = LocationAwareActivityImpl()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        settingsClientManager = SettingsClientManager(this)
-        locationManager = LocationManager(this, settingsClientManager)
+        activityImplementation.onCreate(this, savedInstanceState)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int,
                                             permissions: Array<String>,
                                             grantResults: IntArray) {
-        for (permission in permissions) {
-            LocatorPreferences.writeToPreferences(permission, true)
-        }
-        locationManager.notifyPermissionRequestResults(permissions, grantResults)
+        activityImplementation.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == SettingsClientManager.REQUEST_CODE_SETTINGS) {
-            settingsClientManager.onActivityResult(resultCode)
-        }
+        activityImplementation.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        activityImplementation.onStart(this)
+    }
+
+    override fun onStop() {
+        activityImplementation.onStop(this)
+        super.onStop()
     }
 }
